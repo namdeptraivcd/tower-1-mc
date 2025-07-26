e6e2706c315804f9ed28b21148f3f8bd0b6919aa
namdeptraivcd <namhoang4d@gmail.com>
2025-07-26 20:37:08 +0700

    “Update”

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -1,16 +1,13 @@
-# To support both python 2 and python 3
 from __future__ import division, print_function, unicode_literals
-import numpy as np 
-import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt 
 
-# height (cm)
-X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
-# weight (kg)
-y = np.array([[ 49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68]]).T
-# Visualize data 
+X = np.array([[147, 150, 153, 155, 158, 160, 163, 165, 168, 170, 170, 173, 175, 178, 180, 183]]).T
+
+y = np.array([[49, 50, 51, 52, 54, 56, 58, 59, 60, 72, 63, 64, 66, 67, 68]]).T
+
 plt.plot(X, y, 'ro')
-plt.axis([140, 190, 45, 75])
+plt.axis([140, 190, 40, 80])
 plt.xlabel('Height (cm)')
-plt.ylabel('Weight (kg)')
-plt.savefig('scatter_plot.png')
-plt.show()
+plt.ylabel('weight (kg)')
+plt.savefig('show_data.png')
