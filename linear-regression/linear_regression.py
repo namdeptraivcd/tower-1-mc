6fb7751e645161fe6e35cd4d6480ae7cbf5e9b2e
namdeptraivcd <namhoang4d@gmail.com>
2025-07-26 22:25:25 +0700

    “Update”

--- linear-regression/linear_regression.py
+++ linear-regression/linear_regression.py
@@ -2,12 +2,41 @@ from __future__ import division, print_function, unicode_literals
 import numpy as np
 import matplotlib.pyplot as plt 
 
-X = np.array([[147, 150, 153, 155, 158, 160, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
+X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
 
-y = np.array([[49, 50, 51, 52, 54, 56, 58, 59, 60, 72, 63, 64, 66, 67, 68]]).T
+y = np.array([[49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68]]).T
 
 plt.plot(X, y, 'ro')
 plt.axis([140, 190, 40, 80])
 plt.xlabel('Height (cm)')
 plt.ylabel('weight (kg)')
 plt.savefig('show_data.png')
+
+
+# từ show_data ta thấy rằng linear_regression là một mô hình phù hợp với dữ liệu này
+
+# bắt đầu xây dựng hàm loss và tìm ra hệ số w
+
+one = np.ones((X.shape[0], 1))
+X_bar = np.concatenate ((one, X), axis = 1) # thêm 1 cột để tính hệ số w1
+
+A = np.dot(X_bar.T, X_bar)
+b = np.dot(X_bar.T, y)
+w = np.dot(np.linalg.inv(A), b)
+
+
+# hàm đường regression
+w_0 = w[0][0]
+w_1 = w[1][0]
+x_0 = np.linspace(140, 190, 2)
+y_0 = w_0 + w_1 * x_0
+
+# vẽ đường regression 
+
+plt.plot(X,y, 'ro')
+plt.plot(x_0, y_0)
+plt.axis([140, 190, 40, 80])
+plt.xlabel('Height (cm)')
+plt.ylabel('weight (kg)')
+plt.savefig('output.png')
+
