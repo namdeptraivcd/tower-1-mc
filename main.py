006d3ac45f0edd47dfa71cdeb92d087ac517f985
hoangducanh1865 <hoangducanh.1865@gmail.com>
2025-07-28 13:48:51 +0700

    “Update”

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from src.model import linear_model
 from src.model.linear_model import LinearRegression
+from src.config.config import DEVICE
+
+print(DEVICE)
 
 X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
 y = np.array([[49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68]]).T
