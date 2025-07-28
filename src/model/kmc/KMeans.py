93c02369c51c3746ed41e0e2d56d0988593a2e11
namdeptraivcd <namhoang4d@gmail.com>
2025-07-28 16:26:44 +0700

    “Update”

--- src/model/kmc/KMeans.py
+++ src/model/kmc/KMeans.py
@@ -3,39 +3,29 @@ import matplotlib.pyplot as plt
 
 class KMeans:
     def __init__(self, n_clusters=3, max_iters=100):
-        self.n_clusters = n_clusters
+        self.k = n_clusters
         self.max_iters = max_iters
         self.centroids = None
 
     def fit(self, X):
-        # Initialize centroids randomly from the dataset
-        random_indices = np.random.choice(X.shape[0], self.n_clusters, replace=False)
-        self.centroids = X[random_indices]
-
+        np.random.seed(0)
+        self.centroids = X[np.random.choice(len(X), self.k, replace=False)]
         for _ in range(self.max_iters):
-            # Assign clusters
-            distances = self._compute_distances(X)
-            labels = np.argmin(distances, axis=1)
-
-            # Update centroids
-            new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.n_clusters)])
-            if np.all(new_centroids == self.centroids):
+            labels = self.predict(X)
+            new_centroids = np.array([X[labels == i].mean(axis=0) for i in range(self.k)])
+            if np.allclose(self.centroids, new_centroids):
                 break
             self.centroids = new_centroids
 
-    def _compute_distances(self, X):
-        return np.linalg.norm(X[:, np.newaxis] - self.centroids, axis=2)
-
     def predict(self, X):
-        distances = self._compute_distances(X)
+        distances = np.linalg.norm(X[:, None] - self.centroids, axis=2)
         return np.argmin(distances, axis=1)
 
     def kmeans_display(self, X):
+        if X.shape[1] != 2:
+            raise ValueError("Chỉ hiển thị được dữ liệu 2 chiều.")
         labels = self.predict(X)
-        plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', marker='o')
-        plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red', marker='x', s=200, label='Centroids')
+        plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis')
+        plt.scatter(self.centroids[:, 0], self.centroids[:, 1], c='red', marker='x', s=200)
         plt.title('KMeans Clustering')
-        plt.xlabel('Feature 1')
-        plt.ylabel('Feature 2')
-        plt.legend()
         plt.show()
